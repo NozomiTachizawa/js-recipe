b475076b24847078706e623d931c6c0f71a35959
NozomiTachizawa <mugikko27@gmail.com>
2021-10-31 17:55:17 +0900

    Vue ATM

---
+++ 02_レシピ/03_Vue ATM/main.js
@@ -0,0 +1,27 @@
+const now = new Date();
+
+new Vue({
+    el: '#ATM',
+    data: {
+        nyuryokugaku: 0,
+        zandaka: 0,
+        logs: [],
+    },
+    methods: {
+        nyukin: function () {
+            this.zandaka += Number(this.nyuryokugaku);
+            this.logs.push("日付：" + now, "操作：入金", "金額：" + this.nyuryokugaku + "円");
+        },
+        syukkin: function () {
+            this.zandaka -= Number(this.nyuryokugaku);
+            this.logs.push("日付：" + now, "操作：出金", "金額：" + this.nyuryokugaku + "円");
+        },
+    },
+    computed: {
+        sagaku: function () {
+            let price = 0
+            price = Number(this.nyuryokugaku) - Number(this.zandaka) - 1
+            return (price);
+        },
+    },
+})
